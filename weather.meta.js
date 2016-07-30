0e64085383e46e3480ac8b7743ef2f2d30979f80
Hurqalia <Hurqalia@users.noreply.github.com>
2016-07-30 10:44:28 +0200

    Create weather.meta.js

---
+++ weather.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @id             iitc-plugin-weather
+// @name           IITC plugin: Weather Map
+// @category       Layer
+// @version        0.1.0.20160726.001
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/Hurqalia/weather_map/raw/master/weather.meta.js
+// @downloadURL    https://github.com/Hurqalia/weather_map/raw/master/weather.user.js
+// @installURL     https://github.com/Hurqalia/weather_map/raw/master/weather.user.js
+// @description    [hurqalia22-2016-07-26-001] Weather Map
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
