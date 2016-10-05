e22a527da205124a967f62aad088b0ce8b22c19d
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:30:16 +0200

    Update weather.meta.js
    Fix Niantics redirect

--- weather.meta.js
+++ weather.meta.js
@@ -2,15 +2,19 @@
 // @id             iitc-plugin-weather
 // @name           IITC plugin: Weather Map
 // @category       Layer
-// @version        0.1.7.20160726.001
+// @version        0.1.8.20160726.001
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/weather_map/raw/master/weather.meta.js
 // @downloadURL    https://github.com/Hurqalia/weather_map/raw/master/weather.user.js
 // @installURL     https://github.com/Hurqalia/weather_map/raw/master/weather.user.js
 // @description    [hurqalia22-2016-07-26-001] Weather Map
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
